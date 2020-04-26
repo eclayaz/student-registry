145e8375ad2cb509aa2e8903915a3c77e51e8251
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-04-27 00:18:30 +0530

    change the port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.render(
   <React.StrictMode>
