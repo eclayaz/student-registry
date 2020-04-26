0c41f4fd7848fa699d8c5f6035627506dc5ec59c
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-04-27 04:42:53 +0530

    add jest

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "http://localhost:3000";
 
 ReactDOM.render(
   <React.StrictMode>
